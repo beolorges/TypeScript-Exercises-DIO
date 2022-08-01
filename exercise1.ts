c0a7c54c1d4b852d21c709288cb4393d3f8ac3d6
Leonardo Borges <beolorges@gmail.com>
2022-08-01 14:37:05 -0300

    Exercise 1

--- exercise1.ts
+++ exercise1.ts
@@ -1,4 +1,16 @@
-interface employee {
+interface IEmployee {
   code: number;
   name: string;
 }
+
+class Employee implements IEmployee {
+  code: number;
+  name: string;
+
+  constructor(_code: number, _name: string) {
+    this.code = _code;
+    this.name = _name;
+  }
+}
+
+const employee1 = new Employee(777, "Teste")
